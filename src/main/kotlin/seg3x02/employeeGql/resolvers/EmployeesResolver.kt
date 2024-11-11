e7b24d548d965b98d0a90313f7e2621accb4fc3a
exaucee2 <embuy098@uottawa.ca>
2024-11-10 22:38:09 -0500

    soumission

--- src/main/kotlin/seg3x02/employeeGql/resolvers/EmployeesResolver.kt
+++ src/main/kotlin/seg3x02/employeeGql/resolvers/EmployeesResolver.kt
@@ -1,8 +1,35 @@
 package seg3x02.employeeGql.resolvers
 
+import org.springframework.graphql.data.method.annotation.Argument
+import org.springframework.graphql.data.method.annotation.MutationMapping
+import org.springframework.graphql.data.method.annotation.QueryMapping
 import org.springframework.stereotype.Controller
+import seg3x02.employeeGql.entity.Employee
+import seg3x02.employeeGql.repository.EmployeesRepository
+import seg3x02.employeeGql.resolvers.types.CreateEmployeeInput
+import java.util.*
 
 @Controller
-class EmployeesResolver {
+class EmployeesResolver(private val employeesRepository: EmployeesRepository) {
 
+    @QueryMapping
+    fun employees(): List<Employee> = employeesRepository.findAll()
+
+    @QueryMapping
+    fun employeeById(@Argument id: String): Employee? =
+        employeesRepository.findById(id).orElse(null)
+
+    @MutationMapping
+    fun newEmployee(@Argument createEmployeeInput: CreateEmployeeInput): Employee {
+        val employee = Employee(
+            name = createEmployeeInput.name ?: throw Exception("Name Is Required"),
+            dateOfBirth = createEmployeeInput.dateOfBirth ?: throw Exception("Date Of Birth Required"),
+            city = createEmployeeInput.city ?: throw Exception("City Is Required"),
+            salary = createEmployeeInput.salary ?: throw Exception("Valid Salary Is Required"),
+            gender = createEmployeeInput.gender,
+            email = createEmployeeInput.email
+        )
+        employee.id = UUID.randomUUID().toString()
+        return employeesRepository.save(employee)
+    }
 }
